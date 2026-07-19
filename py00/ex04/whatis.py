6b18f5ea3a0de39ddd5b5b29bcf5ad461164b458
joseph <josephtabia1@gmail.com>
2026-07-19 17:20:28 +0100

    finish ex04 with all errors handing and edge cases

--- py00/ex04/whatis.py
+++ py00/ex04/whatis.py
@@ -1,14 +1,30 @@
 import sys
 
-if len(sys.argv) > 2:
-    print("AssertionError: more than one argument is provided")
-    exit(1)
 
-if sys.argv[1].isdigit() != True:
-    print("AssertionError: argument is not an integer")
-    exit(1)
+def main():
+    # check if the number of arguments is correct
+    if len(sys.argv) < 2:
+        return
+    # check if the argument is an integer
+    if len(sys.argv) > 2:
+        raise AssertionError("more than one argument is provided")
+   # check if the argument is an integer
+    try:
+        num = int(sys.argv[1])
+    except ValueError:
+      # if the argument is not an integer, raise a ValueError
+      # AssertionError is a built in expection in python that is raised when an assertion statement fails
+        raise AssertionError("argument is not an integer")
+    if num % 2 == 0:
+        print("even")
+    else:
+        print("odd")
 
-if int(sys.argv[1]) % 2 == 0:
-    print("I'm Even.")
-else:
-    print("I'm Odd.")
+if __name__ == "__main__":
+    try:
+        main()
+    except AssertionError as e:
+        print(f"AssertionError: {e}")
+
+
+
